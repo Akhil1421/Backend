70a79587b3692b29a686bbef28c01cb7cc07ecea
unknown <akhilsharma1421@gmail.com>
2023-05-30 17:24:32 +0530

    Full

--- app.js
+++ app.js
@@ -7,8 +7,17 @@ const path = require("path")
 
 const port = 4000
 app.use(express.json())
-app.use(express.static(path.join(__dirname, "./frontend/build")))
 app.use("/", router)
+app.use(express.static(path.join(__dirname, "./frontend/build")))
+
+app.get("*", function(_,res){
+    res.sendFile(
+        path.join(__dirname, "./frontend/build/index.html"),
+        function(err){
+            res.status(500).send(err)
+        }
+    )
+})
 const start = async()=>{
     try {
         await connectdb(process.env.MONGO_URI)
