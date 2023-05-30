e94092ce5ff01828ba05de5f64abe04f3e5f2045
unknown <akhilsharma1421@gmail.com>
2023-05-30 17:05:16 +0530

    Api

--- app.js
+++ app.js
@@ -1,23 +1,23 @@
-require("dotenv").config()
+require('dotenv').config()
 const express = require("express")
-const {router} = require("./backend/routes")
 const app = express()
+const {router} = require("./backend/routes")
 const connectdb = require("./backend/connectdb")
-const path = require("path")
+// const path = require("path")
 
 const port = 4000
 app.use(express.json())
 // app.use(express.static(path.join(__dirname, "../frontend/build")))
 app.use("/", router)
-const start  = ()=>{
-    app.listen(port,async()=>{        
-        try {
-            connectdb(process.env.MONGO_URI)
-            console.log(`Server started  at port ${port}`)
-        } catch (error) {
-            console.log(error.message)
-        }
-    })
+const start = async()=>{
+    try {
+        await connectdb(process.env.MONGO_URI)
+        app.listen(port, 
+            console.log(`Listening at the port ${port}...`)
+        )
+    } catch (error) {
+        console.log(error)
+    }
 }
 
-start() 
+start()
