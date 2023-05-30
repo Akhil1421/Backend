489ef2aa90c2f3587cd2d149d6c5744d03ec00f3
unknown <akhilsharma1421@gmail.com>
2023-05-30 17:10:02 +0530

    Api

--- app.js
+++ app.js
@@ -3,11 +3,11 @@ const express = require("express")
 const app = express()
 const {router} = require("./backend/routes")
 const connectdb = require("./backend/connectdb")
-// const path = require("path")
+const path = require("path")
 
 const port = 4000
 app.use(express.json())
-// app.use(express.static(path.join(__dirname, "../frontend/build")))
+app.use(express.static(path.join(__dirname, "../frontend/build")))
 app.use("/", router)
 const start = async()=>{
     try {
